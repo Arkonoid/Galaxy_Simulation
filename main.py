492fb9601b219a87d4a8ab79d60ac8f04c5d816b
Arkonoid <tyler@tylermc.me>
2023-03-29 18:58:52 -0400

    Added comments for a simplified version of the program that should much more easily meet MVP.

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ def random_generator(min_value, max_value):
     return random_number
 
 
-test_number = random_generator(0, 5)
+#Create 'Galaxy' class (shape, number of stars)
+# generate number of stars (75,000,000,000 - 125,000,000,000)
 
+#Create 'Planet' class (size, distance to star, primary biome)
+# generate number of planets based on stars
+
+# Create 'Species' class (id, morphology general, morphology limbs, height, weight, sapience index)
+
+# output a csv file for the planets and the species separately
+
+# use pandas sql to read the two csv files
+
+# join the two tables
 
 # Create 'Galaxy' class
 class Galaxy:
@@ -37,37 +48,47 @@ galaxy_star_count = random_generator(galaxy_stars_options[galaxy_size][0], galax
 # Create a new 'Galaxy' Object using the generated properties
 galaxy1 = Galaxy(galaxy_size_options[galaxy_size], galaxy_shape_options[galaxy_shape], galaxy_star_count)
 
-
 # print(galaxy1.size)
 # print(galaxy1.shape)
 # print("{:,}".format(galaxy1.number_of_stars))
 
 # Create 'Star' class
-star_list = []
-star_class_list = ['O', 'B', 'A', 'F', 'G', 'K', 'M']
-for i in range(1000000):
-    star_class = random.choices(star_class_list, weights=(0.00003, 0.0013, 0.006, 0.03, 0.076, 0.121, 0.7645))
-    star_list.append(star_class[0])
+# star_list = []
+# star_class_list = ['O', 'B', 'A', 'F', 'G', 'K', 'M']
+# for i in range(1000000):
+#     star_class = random.choices(star_class_list, weights=(0.00003, 0.0013, 0.006, 0.03, 0.076, 0.121, 0.7645))
+#     star_list.append(star_class[0])
 
-print(f"System's star is a {star_list}")
 
 # Generate 'Planet' class
 class Planet:
-    def __init__(self, size, shape, number_of_stars):
+    def __init__(self, size, primary_biome, distance_from_star):
         self.size = size
-        self.shape = shape
-        self.number_of_stars = number_of_stars
+        self.primary_biome = primary_biome
+        self.distance_from_star = distance_from_star
         pass
 
+planet_number = galaxy_star_count/1000
+print(planet_number)
+
+habitable_planet_count = planet_number * 0.001
+print(habitable_planet_count)
 
 #   Generate number of planets based on stars
+
 #   Generate distance of planets
 #   Generate size off planets
 #   Generate type of planet
 
 # Generate 'Species' class
 class Species:
-    pass
+    def __init__(self, id, morphology_general, morphology_limbs, height, weight, sapience_index):
+        self.id = id
+        self.morphology_general = morphology_general
+        self.morphology_limbs = morphology_limbs
+        self.height = height
+        self.weight = weight
+        self.sapience_index = sapience_index
 
 #   Generate species based on planet type
 #   Generate species traits
